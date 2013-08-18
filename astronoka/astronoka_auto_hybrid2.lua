6aeaf516f30480d7e49a87c5170c77d34f36b1e4
fortissimo916@gmail.com <fortissimo916@gmail.com@2d8e038d-0863-f0cb-6cdb-81757183843f>
2013-08-18 09:55:37 +0000

    add permutation(), target_tree for 4/26
      - new: permutate target tree
      - new: add isKeepedSeed() for excluding specific seeds
      - add: target tree for 4/16, 4/22, 4/26
      - add: research eponym, attribute of seed
      - add: some input macro

--- astronoka/astronoka_auto_hybrid2.lua
+++ astronoka/astronoka_auto_hybrid2.lua
@@ -27,9 +27,35 @@ require "astronoka_target_vegee"
 -- function
 ------------------------------------------------------------
 
+function perm_generate(a, n)
+	if n == 0 then
+		coroutine.yield(a)
+	else
+		for i=1,n do
+
+			-- put i-th element as the last one
+			a[n], a[i] = a[i], a[n]
+
+			-- generate all permutations of the other elements
+			perm_generate(a, n - 1)
+
+			-- restore i-th element
+			a[n], a[i] = a[i], a[n]
+
+		end
+	end
+end
+
+function permutation(a)
+	local n = table.getn(a)
+	return coroutine.wrap(function () perm_generate(a, n) end)
+end
+
+
+
 function convert2Target(tree)
 	local target = {}
-	-- if tree.right is a node, switch parent seed because of cursor moving
+	-- if tree.right is a node, switch parent seed due to reduce cursor moving
 	if tree.right.left ~= nil and tree.right.right ~= nil then
 		target.first = tree.right.property
 		target.second = tree.left.property
@@ -43,7 +69,7 @@ function convert2Target(tree)
 	return target
 end
 
-function hybridPo(tree)
+function hybridizePo(tree)
 	local target = convert2Target(tree)
 	--print(target)
 	Hybrid.autoHybrid(target)
@@ -65,6 +91,92 @@ function postorder(tree, func)
 	end
 end
 
+function hybridizeTree(tree)
+	Hybrid.keepedSeed = {}
+	for i=1, #tree, 1 do
+		postorder(tree[i], hybridizePo)
+		-- keep hybridized seed
+		table.insert(Hybrid.keepedSeed, Hybrid.sd4)
+	end
+end
+
+
+function hybridizeTreePerm(tree)
+	local state_tmp = savestate.create()
+	local state_best = savestate.create()
+	local elapsed_fc_best = 0
+
+	-- save start state
+	savestate.save(state_tmp)
+
+	for p in permutation(tree) do
+		-- load start state
+		savestate.load(state_tmp)
+
+		target_tree_summary = {}
+		local start_total_reset = memory.readword(adr_total_reset_cnt)
+		local start_date = os.date()
+		local start_fc = emu.framecount()
+
+		hybridizeTree(p)
+		outputSummary(start_total_reset, start_date, start_fc)
+
+		local elapsed_fc = emu.framecount() - start_fc
+		print(string.format("elapsed_fc = %d, elapsed_fc_best = %d", elapsed_fc, elapsed_fc_best))
+		if elapsed_fc < elapsed_fc_best or elapsed_fc_best == 0 then
+			-- save best state
+			savestate.save(state_best)
+			print("created state_best")
+
+			elapsed_fc_best = elapsed_fc
+		end
+	end
+
+	-- load best state
+	savestate.load(state_best)
+end
+
+
+function getSummary(sreset, sdate, sfc, fname)
+	local tbl = {}
+	local total_lr = 0
+	local total_reset = memory.readword(adr_total_reset_cnt)
+	local fc = emu.framecount()
+
+	table.insert(tbl, ">>> print Summary >>>")
+	for i=1, #target_tree_summary, 1 do
+		table.insert(tbl, "summary:: "..target_tree_summary[i].str)
+		total_lr = total_lr + target_tree_summary[i].retry
+	end
+
+	table.insert(tbl, string.format(">>> print elapsed >>>"))
+	table.insert(tbl, string.format("total retry:: %d, %d", total_reset - sreset, total_lr))
+	table.insert(tbl, string.format("  start:: %s,  fc = %d", sdate, sfc))
+	table.insert(tbl, string.format("    end:: %s,  fc = %d", os.date(), fc))
+	table.insert(tbl, string.format("elapsed:: fc = %d", fc - sfc))
+	table.insert(tbl, string.format("<<< hybrid is finished <<<"))
+	return tbl
+end
+
+function printSummary(sreset, sdate, sfc, fname)
+	local tbl = getSummary(sreset, sdate, sfc, fname)
+	for i=1, #tbl, 1 do
+		print(tbl[i])
+	end
+end
+
+function outputSummary(sreset, sdate, sfc)
+	local fp = io.open("auto_hybrid2.log", "a")
+	if fp == nil then print("error: Could not open file") end
+
+	local tbl = getSummary(sreset, sdate, sfc, fname)
+	for i=1, #tbl, 1 do
+		fp:write(tbl[i] .. "\n")
+	end
+
+	fp:flush()
+	fp:close()
+end
 
 ------------------------------------------------------------
 -- main
@@ -96,7 +208,9 @@ target_vegee = target_vegee_1305
 --target_tree = target_tree_1401
 --target_tree = target_tree_1401_typeB
 --target_tree = target_tree_1408
-target_tree = target_tree_1416
+--target_tree = target_tree_1416
+--target_tree = target_tree_1422
+target_tree = target_tree_1426
 
 try_cnt1 = 1000  -- max retry count each level up
 try_cnt2 = 24    -- max level up count
@@ -104,11 +218,12 @@ try_cnt2 = 24    -- max level up count
 
 
 target_tree_summary = {}
-total_retry = 0
+total_lr = 0  -- @deprecated
 initial = 1
-local begin_fc = emu.framecount()
-local begin_date = os.date()
-local begin_total_reset = memory.readword(adr_total_reset_cnt)
+init_total_reset = memory.readword(adr_total_reset_cnt)
+init_date = os.date()
+init_fc = emu.framecount()
+
 while true do
 
 	if initial == 1 then
@@ -130,17 +245,16 @@ while true do
 		--end
 		--for i=1, #target_vegee, 1 do
 		--	print("summary:: "..targetToSummaryString(target_vegee[i]))
-		--	total_retry = total_retry + target_vegee[i].retry
+		--	total_lr = total_lr + target_vegee[i].retry
 		--end
 
 		-- # use binary tree
-		for i=1, #target_tree, 1 do
-			postorder(target_tree[i], hybridPo)
-		end
-		for i=1, #target_tree_summary, 1 do
-			print("summary:: "..target_tree_summary[i].str)
-			total_retry = total_retry + target_tree_summary[i].retry
-		end
+		hybridizeTree(target_tree)
+		printSummary(init_total_reset, init_date, init_fc)
+
+		-- # permutate target_tree
+		--hybridizeTreePerm(target_tree)
+		
 		break
 	end
 
@@ -149,11 +263,12 @@ end
 
 local fc = emu.framecount()
 local total_reset = memory.readword(adr_total_reset_cnt)
-print(string.format("<<< lua bot is finished <<<"))
-print(string.format("total retry:: %d, %d", total_reset - begin_total_reset, total_retry))
-print(string.format("  start:: %s,  fc = %d", begin_date, begin_fc))
+print(string.format(">>> print elapsed >>>"))
+print(string.format("total retry:: %d, %d", total_reset - init_total_reset, total_lr))
+print(string.format("  start:: %s,  fc = %d", init_date, init_fc))
 print(string.format("    end:: %s,  fc = %d", os.date(), fc))
-print(string.format("elapsed:: fc = %d", fc - begin_fc))
+print(string.format("elapsed:: fc = %d", fc - init_fc))
+print(string.format("<<< lua bot is finished <<<"))
 emu.speedmode("normal")
 emu.pause()
 
