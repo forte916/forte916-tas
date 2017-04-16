f91bdbe461ec3dfc6a83b9c8e1d1353cce8cd21f
Shinichi Noma <fortissimo916@gmail.com>
2017-04-17 01:12:14 +0900

    new: neoatlas2 libs

---
+++ neoatlas2/na2_lib.lua
@@ -0,0 +1,22 @@
+
+city_offset = 0x801AED60 -- 0x40 each
+
+city_jedda        = 0x801AED60	-- city_offset
+city_alecsandria  = 0x801AEDA0	-- city_offset + (0x40 * 1)
+city_chunis       = 0x801AEDE0	-- city_offset + (0x40 * 2)
+city_toripoli     = 0x801AEE20	-- city_offset + (0x40 * 3)
+city_lisbon       = 0x801AEE60	-- city_offset + (0x40 * 4)
+city_london       = 0x801AEEA0	-- city_offset + (0x40 * 5)
+
+
+off_xxxx              = 0x00
+off_city_id           = 0x02
+off_population        = 0x04		-- x 100
+off_1st_product_id    = 0x10
+off_1st_product_stock = 0x12		-- 0(0%) - 0x05DC(100%)
+off_1st_product_id    = 0x18
+off_1st_product_stock = 0x1A		-- 0(0%) - 0x05DC(100%)
+off_1st_product_id    = 0x20
+off_1st_product_stock = 0x22		-- 0(0%) - 0x05DC(100%)
+
+
